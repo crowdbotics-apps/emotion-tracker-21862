6d6f0f4cde087fb2e7d15ff247e48e224d641747
Crowdbotics <team@crowdbotics.com>
2020-10-23 06:28:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/emotion_tracker_21862/MainActivity.java
@@ -0,0 +1,15 @@
+package com.emotion_tracker_21862;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "emotion_tracker_21862";
+    }
+}
