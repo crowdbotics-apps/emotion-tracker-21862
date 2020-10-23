0e26066d32d9e9918100fb05becec3601cd549dd
Crowdbotics <team@crowdbotics.com>
2020-10-23 06:36:42 +0000

    Update models

--- backend/emotion_tracker_21862/settings.py
+++ backend/emotion_tracker_21862/settings.py
@@ -47,6 +47,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "healthcare",
+    "task",
+    "task_profile",
+    "tasker_business",
+    "location",
+    "wallet",
+    "task_category",
 ]
 LOCAL_APPS = [
     "home",
