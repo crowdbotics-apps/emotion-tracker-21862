16b7efb9b4dcde58f0a58d5140d5883cedd7bb5a
Crowdbotics <team@crowdbotics.com>
2020-10-23 06:30:42 +0000

    Update models

--- backend/emotion_tracker_21862/wsgi.py
+++ backend/emotion_tracker_21862/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emotion_tracker_21862.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emotion_tracker_21862.settings")
 
 application = get_wsgi_application()
